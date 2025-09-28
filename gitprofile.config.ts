8a2a3e2425a1c53ce52a39f37e1ec06451675cdb
Ratchanon Pawanja <68798300+march250602@users.noreply.github.com>
2025-09-28 09:46:30 +0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -154,14 +154,7 @@ const CONFIG = {
     ],
   },
 
-  // Optional Footer. Supports plain text or HTML.
-  // footer: `Made with <a 
-  //     class="text-primary" href="https://github.com/arifszn/gitprofile"
-  //     target="_blank"
-  //     rel="noreferrer"
-  //   >GitProfile</a> and ❤️`,
-
-  // enablePWA: true,
+  enablePWA: true,
 };
 
 export default CONFIG;
